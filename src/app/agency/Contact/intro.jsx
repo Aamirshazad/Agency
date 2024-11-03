a5c0c74dd903cf6c4da2e4186585bc77c8b229d9
Aamirshazad <amiralicomsats3@gmail.com>
2024-11-03 20:24:58 +0500

    update

---
+++ src/app/agency/Contact/intro.jsx
@@ -0,0 +1,19 @@
+export default function Intro(){
+    return(
+        <div className="mt-48 ml-16">
+            <div style={{ opacity: 1, transform: 'none' }}>
+    <h1>
+        <span className="block font-display text-base font-semibold text-neutral-950">Contact us</span>
+        <span className="sr-only"> - </span>
+        <span className="mt-6 block max-w-5xl font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-6xl">Let’s work together</span>
+    </h1>
+    <div className="mt-6 max-w-3xl text-xl text-neutral-600">
+        <p>We can’t wait to hear from you.</p>
+    </div>
+</div>
+
+
+
+        </div>
+    )
+}
